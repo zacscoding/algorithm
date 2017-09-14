ec72fb8a21d60cc1089cc907b671771c33106c2d
zaccoding <zaccoding725@gmail.com>
2017-09-14 10:19:50 +0900

    solve dp & graph
    - dp : 9095, 11727
    - graph : 1707

--- acm/src/chap08/dp/p11727/Try.java
+++ acm/src/chap08/dp/p11727/Try.java
@@ -1,4 +1,4 @@
-package chap27.graph.p1707;
+package chap08.dp.p11727;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,17 +7,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * https://www.acmicpc.net/problem/1707
+ * https://www.acmicpc.net/problem/11727
  * 
  * @author zaccoding
- * @Date 2017. 9. 13.
+ * @Date 2017. 9. 14.
  */
-public class Try {    
+public class Try {
+    public static int n;
+    public static final int mod = 10007;
+    public static int[] dp = new int[1001];    
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Reader.init(System.in);        
+        n = Reader.nextInt();        
+        dp[1] = 1;
+        dp[2] = 3;
+        System.out.println(solve(n));
     }
     
+    public static int solve(int n) {
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+        
+        return dp[n] = (solve(n-1) + (2*solve(n-2))) % mod;
+    }
+    
+
     static class Reader {
         static BufferedReader reader;
         static StringTokenizer tokenizer;       
@@ -25,7 +41,6 @@ public class Try {
         public static void init(InputStream input) {
             reader = new BufferedReader(new InputStreamReader(input));
         }
-        
         
         public static String nextLine() throws IOException {
             return reader.readLine();
@@ -40,6 +55,12 @@ public class Try {
         
         public static int nextInt() throws IOException {
             return Integer.parseInt( next() );
-        }               
+        }
+        
+        public static void close() throws IOException {
+            if(reader != null)
+                reader.close();
+        }
+        
     }
 }

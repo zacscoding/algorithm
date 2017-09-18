f3eec10b84a7e3861dbf96ba9ff11f589b6d4f90
mskim <mskim.comtrue@gmail.com>
2017-09-18 10:25:36 +0900

    dp 11052

---
+++ acm/src/chap08/dp/p11052/Try2.java
@@ -0,0 +1,49 @@
+package chap08.dp.p11052;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Try2 {
+    static int[] dp = new int[1001];    
+    public static void main(String[] args) throws IOException {
+        Reader.init(System.in);        
+        int N = Reader.nextInt();
+        int[] p = new int [N+1];
+        for(int i=1; i<=N; i++) {
+            p[i] = Reader.nextInt();
+            dp[i] = p[i];
+            for(int j=1; j<i; j++) {
+                dp[i] = Math.max(dp[i],p[j] + dp[i-j]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;       
+        
+        public static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+        }
+        
+        
+        public static String nextLine() throws IOException {
+            return reader.readLine();
+        }
+        
+        public static String next() throws IOException {
+            while(tokenizer ==null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();           
+        }
+        
+        public static int nextInt() throws IOException {
+            return Integer.parseInt( next() );
+        }               
+    }
+}

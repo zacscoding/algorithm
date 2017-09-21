55b0f2471fcf2ed3b23b9b1c66f298165fa7e874
zaccoding <zaccoding725@gmail.com>
2017-09-21 10:06:30 +0900

    [dp] 10844

--- acm/src/chap08/dp/p10844/Main.java
+++ acm/src/chap08/dp/p10844/Main.java
@@ -13,12 +13,32 @@ import java.util.StringTokenizer;
  * @author zaccoding
  * @Date 2017. 9. 20.
  */
-public class Try {    
-    static int[] dp = new int[101];
+public class Main {    
+    // dp[i][j] == 길이 i 인 경우, j의 개수
+    static long[][] dp = new long[101][10];
+    static final int mod = 1000000000;
     public static void main(String[] args) throws IOException {
         Reader.init(System.in);
-        int N = Reader.nextInt();       
+        int N = Reader.nextInt();
+        for(int i=1; i<=9; i++) {
+            dp[1][i] = 1L;
+        }
         
+        for(int i=2; i<=N; i++) {
+            for(int j=0; j<=9; j++) {
+                dp[i][j] = 0;
+                if(j-1 >= 0) dp[i][j] += dp[i-1][j-1];
+                if(j+1 <= 9) dp[i][j] += dp[i-1][j+1];                
+                dp[i][j] %= mod; 
+            }
+        }
+        
+        long answer = 0;
+        for(int i=0; i<=9; i++) {
+            answer += dp[N][i];
+        }
+         
+        System.out.println(answer%mod);
     }
     
     static class Reader {
